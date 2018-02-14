f8bcd1e181cc719573bf918ea6ea62af5db0d774
John Owen <john.owen@aginicx.com>
2018-02-14 10:00:29 +1000

    remove incorrect test

--- src/components/RequestTool/simpleHeadersToString.test.js
+++ src/components/RequestTool/simpleHeadersToString.test.js
@@ -1,11 +1,6 @@
-// @flow
 import simpleHeadersToString from "./simpleHeadersToString";
 
 describe("simpleHeadersToString", () => {
-  it("should convert no headers to an empty string", () => {
-    expect(simpleHeadersToString({})).toBe("");
-  });
-
   it("should convert a single header to a string", () => {
     expect(simpleHeadersToString({ host: "example.com" })).toBe(
       "host: example.com"
